760e5afa6496b39ac4f37d1d2fc66f4bdf928c7d
Gregory Halverson <gregory.halverson@gmail.com>
2024-12-04 19:13:15 -0800

    preparing for initial release

--- VIIRS_tiled_granules/VIIRS_tiled_granules.py
+++ VIIRS_tiled_granules/VIIRS_tiled_granules.py
@@ -1,5 +1,5 @@
 from .constants import *
 from .concept_ID_from_DOI import *
 from .VIIRS_tiled_granule import *
-from .VIIRS_CMR_login import *
-from .VIIRS_CMR_query import *
+from .authenticate import *
+from .search_granules import *
